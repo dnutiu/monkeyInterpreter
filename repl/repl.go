a371047c362b76e46bde39ec4103fdf9ca432512
Denis Nuțiu <denis.nutiu@gmail.com>
2018-03-04 13:14:41 +0200

    Implementing the parser

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,7 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/metonimie/monkeyInterpreter/lexer"
-	"github.com/metonimie/monkeyInterpreter/token"
+	"github.com/metonimie/monkeyInterpreter/parser"
 )
 
 const PROMPT = ">"
@@ -14,20 +14,29 @@ func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
-		fmt.Fprintf(out, "%s", PROMPT)
+		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			goto exit
 		}
-
 		line := scanner.Text()
-
 		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "+%v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 	}
+
 exit:
 	return
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
 }
